79e108838c065d6485f718a753f2c76e873e6407
yhfudev <yhfudev@gmail.com>
2020-02-16 21:30:48 -0500

    update script

--- submodule-update.sh
+++ submodule-update.sh
@@ -5,7 +5,9 @@ echo "update MenuSystem submodules ..."
 # lib/libosporting/update-submod.sh
 
 echo "update MenuSystem ..."
-git checkout master
+#git checkout master
+#git branch --set-upstream-to=remotes/yhfudev/devel-pc-ext devel-pc-ext
+git checkout devel-pc-ext
 git pull
 
 #git status lib/libosporting
